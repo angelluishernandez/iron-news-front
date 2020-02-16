845c080ec7357af8fcf51a167a8cb3d95202fae4
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-02-16 11:46:00 +0100

    Login on progress

--- src/components/misc/Login.js
+++ src/components/misc/Login.js
@@ -1,4 +1,62 @@
 import React from "react"
 import IronNewsService from "../../services/IronNewsService"
 import {Link, Redirect} from "react-router-dom"
-import {WithAuthConsumer} from "../../con"
+import {WithAuthConsumer} from "../../contexts/AuthContext"
+
+
+class Login extends React.Component{
+
+  state = {
+    data: {
+      email: "", 
+      password: ""
+    }, 
+    error: false, 
+    loading: false
+  }
+
+  handleSubmit = event => {
+
+  }
+
+  handleBlur = event =>{
+
+
+  }
+
+  handleChange = event =>{
+
+
+
+  }
+
+  render(){
+    const {data, error, loading} = this.state
+    return(
+      <div className="Login"> 
+        <form className="mb-4" onSubmit={this.handleSubmit}>
+          <div className="mb-4">
+        <label htmlFor="email">
+          Email
+        </label>
+        <input type="text" className="form-control" id="email" autoComplete="off" value={data.email} onBlur={this.handleBlur} onChange={this.handleChange} placeholder="Email"/>
+        </div>
+        <div className="mb-4">
+        <label htmlFor="password">
+          Email
+        </label>
+        <input type="password" className="form-control" id="password" autoComplete="off" value={data.password} onBlur={this.handleBlur} onChange={this.handleChange} placeholder="Password"/>
+        </div>
+        </form>
+
+
+
+      </div>
+
+
+
+
+    )
+  }
+}
+
